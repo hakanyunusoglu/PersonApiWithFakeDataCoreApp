63a2563efe5fe1cb073a669564c8f2643cd73b25
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-03-04 12:11:18 +0300

    Add project files.

---
+++ PersonApiWithFakeDataCoreApp/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PersonApiWithFakeDataCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [Route("LoadPersonList")]
+        public List<Person> LoadPersonList()
+        {
+            List<Person> resultlist = new List<Person>();
+            for (int i = 1; i <= 100; i++)
+            {
+                Person person = new Person()
+                {
+                    PersonID = i,
+                    PersonName = FakeData.NameData.GetFirstName(),
+                    PersonSurname = FakeData.NameData.GetSurname(),
+                    PersonEmail = FakeData.NetworkData.GetEmail(),
+                    PersonAdress = FakeData.PlaceData.GetAddress(),
+                    PersonCity = FakeData.PlaceData.GetCity(),
+                    PersonCountry = FakeData.PlaceData.GetCountry()
+                };
+                resultlist.Add(person);
+            }
+            return resultlist;
+        }
+    }
+}
---
+++ PersonApiWithFakeDataCoreApp/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonApiWithFakeDataCoreApp
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string PersonName { get; set; }
+        public string PersonSurname { get; set; }
+        public string PersonEmail { get; set; }
+        public string PersonAdress { get; set; }
+        public string PersonCity { get; set; }
+        public string PersonCountry { get; set; }
+    }
+}
